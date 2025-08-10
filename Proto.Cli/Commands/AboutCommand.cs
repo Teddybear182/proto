146269c7e2fb017fa4397adb8a9be1f1f2004a7f
it1shka <2tishbel@gmail.com>
2025-08-10 17:25:05 +0200

    feat(Proto.Cli): added `about` command

--- Proto.Cli/Commands/AboutCommand.cs
+++ Proto.Cli/Commands/AboutCommand.cs
@@ -1,11 +1,32 @@
+
 namespace Proto.Cli.Commands;
 
 using System.ComponentModel;
+using Proto.Cli.Utils;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 [Description("Shows general information about the language and the CLI tool.")]
 internal sealed class AboutCommand : Command {
   public override int Execute(CommandContext context) {
-    throw new NotImplementedException();
+    var projectInfo = ProjectInformation.ReadProjectInformationFromAssembly();
+
+    var productNameText = new FigletText(projectInfo.ProductName)
+      .Color(Color.Purple);
+    var versionAndAuthorsText = new Panel($"[blue]Version: {projectInfo.Version}, by {projectInfo.Authors}[/]")
+      .Border(BoxBorder.Ascii)
+      .BorderColor(Color.Blue);
+    var descriptionText = new Panel(projectInfo.Description)
+      .Padding(2, 2)
+      .Border(BoxBorder.None);
+    var repositoryUrlText = new Panel($"[blue]Repository: [underline][bold]{projectInfo.RepositoryUrl}[/][/][/]")
+      .Border(BoxBorder.Ascii)
+      .BorderColor(Color.Blue);
+
+    AnsiConsole.Write(productNameText);
+    AnsiConsole.Write(versionAndAuthorsText);
+    AnsiConsole.Write(descriptionText);
+    AnsiConsole.Write(repositoryUrlText);
+    return 0;
   }
 }
