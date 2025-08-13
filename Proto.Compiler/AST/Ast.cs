acc6193e7337da6c5b9532f9724541d743a799ec
FyodorBelousov <teddybearfedor@gmail.com>
2025-08-13 23:30:02 +0200

    feat(Proto.Compiler): rewrite AST class

--- Proto.Compiler/AST/Ast.cs
+++ Proto.Compiler/AST/Ast.cs
@@ -1,6 +1,6 @@
 namespace Proto.Compiler.AST;
 
-public enum NodeType { // enum for node types
+public enum NodeType {
   BinaryExpression,
   UnaryExpression,
   AssignmentExpression,
@@ -21,7 +21,7 @@ public enum NodeType { // enum for node types
   ReturnStatement,
 }
 
-public enum Type { // enum for types
+public enum Type {
   Integer,
   Float,
   String,
@@ -29,121 +29,73 @@ public enum Type { // enum for types
   Boolean,
 }
 
-public abstract record Node() { // main node
-  public readonly NodeType Type;
-}
+public abstract record Node(NodeType Type); // main node, placeholder for every node
 
-public abstract record Program : Node {
-  public readonly NodeType Type = NodeType.Program;
-  public readonly Statement[] Body = [];
-}
-
-public abstract record Statement : Node { // statement and expression nodes are just placeholders for other nodes
-}
-
-public abstract record Expression : Node {
-}
+// statement and expression nodes are just placeholders for other nodes
+public abstract record Statement(NodeType Type) : Node(Type);
+public abstract record Expression(NodeType Type) : Node(Type);
 
 
 //Statements
-public record FuncDeclaration : Statement {
-  public readonly NodeType Type = NodeType.FuncDeclaration;
-  public readonly string Name;
-  public readonly Argument[]? Arguments; // there may be no args
-  public readonly NodeType? ReturnType; // if there is no return type, then its considered void
-  public readonly BlockStatement Body;
-}
+public abstract record Program(Statement[] Body) : Node(NodeType.Program);
 
-public record Argument : Statement { // argument node that contains name and type
-  public readonly string Name;
-  public readonly NodeType Type;
-}
+public record FuncDeclaration(
+  string Name,
+  Argument[]? Arguments,
+  NodeType? ReturnType,
+  BlockStatement Body
+) : Statement(NodeType.FuncDeclaration);
 
-public record VarDeclaration : Statement {
-  public readonly NodeType Type = NodeType.VarDeclaration;
-  public readonly string Name;
-  public readonly bool IsConstant;
-  public readonly Type VarType;
-  public readonly Expression? InitValue; // there may be no initial value
-}
+public record Argument( // argument node that contains name and type
+  string Name,
+  NodeType ArgType
+);
 
-public record BlockStatement : Statement {
-  public readonly NodeType Type = NodeType.BlockStatement;
-  public readonly Statement[] Body = [];
-}
+public record VarDeclaration(
+  string Name,
+  bool IsConstant,
+  Type VarType,
+  Expression? InitValue
+) : Statement(NodeType.VarDeclaration);
 
-public record IfStatement : Statement {
-  public readonly NodeType Type = NodeType.IfStatement;
-  public readonly Expression Condition;
-  public readonly Statement Body;
-}
+public record BlockStatement(Statement[] Body) : Statement(NodeType.BlockStatement);
 
-public record LoopStatement : Statement {
-  public readonly NodeType Type = NodeType.LoopStatement;
-  public readonly Expression Condition;
-  public readonly Statement Body;
-}
+public record IfStatement(
+  Expression Condition,
+  Statement Body
+) : Statement(NodeType.IfStatement);
 
-public record BreakStatement : Statement {
-  public readonly NodeType Type = NodeType.BreakStatement;
-}
+public record LoopStatement(
+  Expression Condition,
+  Statement Body
+) : Statement(NodeType.LoopStatement);
 
-public record ContinueStatement : Statement {
-  public readonly NodeType Type = NodeType.ContinueStatement;
-}
+public record BreakStatement() : Statement(NodeType.BreakStatement);
 
-public record ReturnStatement : Statement {
-  public readonly NodeType Type = NodeType.ReturnStatement;
-  public readonly Expression Value;
-}
+public record ContinueStatement() : Statement(NodeType.ContinueStatement);
+
+public record ReturnStatement(Expression Value) : Statement(NodeType.ReturnStatement);
+
 
 //Expressions
-public record BinaryExpression : Expression {
-  public readonly NodeType Type = NodeType.BinaryExpression;
-  public readonly string Operator;
-  public readonly Expression Left;
-  public readonly Expression Right;
-}
+public record BinaryExpression(
+  string Operator,
+  Expression Left,
+  Expression Right
+) : Expression(NodeType.BinaryExpression);
 
-public record UnaryExpression : Expression {
-  public readonly NodeType Type = NodeType.UnaryExpression;
-  public readonly string Operator;
-  public readonly Expression Operand;
-}
+public record UnaryExpression(string Operator, Expression Operand) : Expression(NodeType.UnaryExpression);
 
-public record AssignmentExpression : Expression {
-  public readonly NodeType Type = NodeType.AssignmentExpression;
-  public readonly Expression Left;
-  public readonly Expression Right;
-}
+public record AssignmentExpression(Expression Left, Expression Right) : Expression(NodeType.AssignmentExpression);
 
-public record CallExpression : Expression {
-  public readonly NodeType Type = NodeType.CallExpression;
-  public readonly Expression Caller;
-  public readonly Expression Arguments;
-}
+public record CallExpression(Expression Caller, Expression Arguments) : Expression(NodeType.CallExpression);
 
-public record IntegerLiteral : Expression {
-  public readonly NodeType Type = NodeType.IntegerLiteral;
-  public readonly int Value;
-}
+public record IntegerLiteral(int Value) : Expression(NodeType.IntegerLiteral);
 
-public record FloatLiteral : Expression {
-  public readonly NodeType Type = NodeType.FloatLiteral;
-  public readonly float Value;
-}
+public record FloatLiteral(float Value) : Expression(NodeType.FloatLiteral);
 
-public record StringLiteral : Expression {
-  public readonly NodeType Type = NodeType.StringLiteral;
-  public readonly string Value;
-}
+public record StringLiteral(string Value) : Expression(NodeType.StringLiteral);
 
-public record CharLiteral : Expression {
-  public readonly NodeType Type = NodeType.CharLiteral;
-  public readonly char Value;
-}
+public record CharLiteral(char Value) : Expression(NodeType.CharLiteral);
 
-public record BooleanLiteral : Expression {
-  public readonly NodeType Type = NodeType.BooleanLiteral;
-  public readonly bool Value;
-}
+public record BooleanLiteral(bool Value) : Expression(NodeType.BooleanLiteral);
