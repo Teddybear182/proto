5a1062a843ddf9b2108fc6e81284cc0c847eacf1
it1shka <2tishbel@gmail.com>
2025-08-15 17:19:27 +0200

    feat(Proto.Cli): added basic repl

--- Proto.Cli/Commands/ReplCommand.cs
+++ Proto.Cli/Commands/ReplCommand.cs
@@ -7,8 +7,14 @@ using Spectre.Console.Cli;
 
 [Description("Launches language interactive shell.")]
 internal sealed class ReplCommand : Command {
+  private readonly List<string> _history = new List<string>();
+
   public override int Execute(CommandContext context) {
     PrintCompilerInformation();
+    var isReplRunning = true;
+    while (isReplRunning) {
+      isReplRunning = this.PromptAndProcess();
+    }
     return 0;
   }
 
@@ -21,5 +27,27 @@ internal sealed class ReplCommand : Command {
       .Border(BoxBorder.None);
     AnsiConsole.Write(compilerNameText);
     AnsiConsole.Write(compilerVersionText);
+  }
+
+  private bool PromptAndProcess() {
+    // TODO: this method should be changed in the future
+    this.PrintReplInputHeader();
+    var userInput = AnsiConsole.Prompt(
+      new TextPrompt<string>("[orange1] >[/]")
+    );
+    if (userInput.Trim() == ":exit") {
+      return false;
+    }
+    // TODO: process input
+    this._history.Add(userInput);
+    return true;
+  }
+
+  private void PrintReplInputHeader() {
+    var index = this._history.Count;
+    var headerElement = new Rule($"Prompt ({index})")
+      .RuleStyle(Color.Blue)
+      .Justify(Justify.Left);
+    AnsiConsole.Write(headerElement);
   }
 }
