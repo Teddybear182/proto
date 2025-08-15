8e591e6616f6c8e38a0adaca3014ccfd3a399fae
it1shka <2tishbel@gmail.com>
2025-08-15 19:35:50 +0200

    feat(Proto.Compiler): added IPeekable and Peekable utils

---
+++ Proto.Compiler/Utils/Peekable.cs
@@ -0,0 +1,25 @@
+namespace Proto.Compiler.Utils;
+
+internal abstract class Peekable<T> : IPeekable<T> {
+#pragma warning disable
+  private T _current;
+#pragma warning restore
+
+  private bool _shouldUpdate = true;
+
+  protected abstract T ReadElement();
+
+  public T Peek() {
+    if (this._shouldUpdate) {
+      this._current = this.ReadElement();
+      this._shouldUpdate = false;
+    }
+    return this._current;
+  }
+
+  public T Next() {
+    var value = this.Peek();
+    this._shouldUpdate = true;
+    return value;
+  }
+}
