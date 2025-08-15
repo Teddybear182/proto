79d128492972e5d28b52d31c46c7ba56ddedfa33
it1shka <2tishbel@gmail.com>
2025-08-15 19:50:35 +0200

    fix(Proto.Compiler): reworked Token and TokenType

--- Proto.Compiler/Lexer/TokenType.cs
+++ Proto.Compiler/Lexer/TokenType.cs
@@ -1,17 +1,36 @@
 namespace Proto.Compiler.Lexer;
 
 public enum TokenType {
+  // literals
   IntegerLiteral,
   FloatLiteral,
   StringLiteral,
   CharLiteral,
   BooleanLiteral,
+  // main token types
   Identifier,
   Operator,
   Keyword,
   Punctuation,
-  Type,
-  EOF,
-  Undefined,
+  TypeLiteral,
+  // special token types
+  Eof,
   Illegal,
+}
+
+public static class TokenTypeExtensions {
+  public static string ShortDescription(this TokenType tokenType) => tokenType switch {
+    TokenType.IntegerLiteral => "integer literal",
+    TokenType.FloatLiteral => "float literal",
+    TokenType.StringLiteral => "string literal",
+    TokenType.CharLiteral => "char literal",
+    TokenType.BooleanLiteral => "boolean literal",
+    TokenType.Identifier => "identifier",
+    TokenType.Operator => "operator",
+    TokenType.Keyword => "keyword",
+    TokenType.Punctuation => "punctuation",
+    TokenType.TypeLiteral => "type literal",
+    TokenType.Eof => "end of file",
+    TokenType.Illegal => "illegal token"
+  };
 }
