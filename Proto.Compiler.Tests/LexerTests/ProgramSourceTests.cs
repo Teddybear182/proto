5363e9bac6e360d4249026af3ff6f461688df973
it1shka <2tishbel@gmail.com>
2025-09-14 12:25:58 +0200

    feat(Proto.Compiler.Tests): added location tests for ProgramSource

--- Proto.Compiler.Tests/LexerTests/ProgramSourceTests.cs
+++ Proto.Compiler.Tests/LexerTests/ProgramSourceTests.cs
@@ -1,9 +1,12 @@
 namespace Proto.Compiler.Tests.LexerTests;
 
 using Proto.Compiler.Lexer;
+using Proto.Compiler.Utils;
 
 [TestFixture]
 public class ProgramSourceTests {
+  #region Implementation Cases
+
   [TestCase("test string")]
   public void ProgramSource_ShouldTraverseCorrectly(string input) {
     var source = new ProgramSource(new StringReader(input));
@@ -57,4 +60,60 @@ public class ProgramSourceTests {
       });
     });
   }
+
+  #endregion
+
+  #region Location Cases
+
+  [TestCase("while")]
+  public void ProgramSource_ShouldHandleLocations_OneLine(string input) {
+    AssertLocations(input, [
+      (Line: 1, Column: 1, Offset: 0),
+      (Line: 1, Column: 2, Offset: 1),
+      (Line: 1, Column: 3, Offset: 2),
+      (Line: 1, Column: 4, Offset: 3),
+      (Line: 1, Column: 5, Offset: 4)
+    ]);
+  }
+
+  [TestCase("a\n\tb")]
+  public void ProgramSource_ShouldHandleLocations_MultiLine(string input) {
+    AssertLocations(input, [
+      (Line: 1, Column: 1, Offset: 0),
+      (Line: 1, Column: 2, Offset: 1),
+      (Line: 2, Column: 1, Offset: 2),
+      (Line: 2, Column: 2, Offset: 3)
+    ]);
+  }
+
+  #endregion
+
+  #region Helper Methods
+
+  private static void AssertLocations(string input, IEnumerable<(uint Line, uint Column, uint Offset)> expectedLocations) {
+    var locations = ReadLocations(input).ToArray();
+    var expected = expectedLocations.ToArray();
+    Assert.Multiple(() => {
+      Assert.That(locations, Has.Length.EqualTo(expected.Length));
+      for (var i = 0; i < expected.Length; i++) {
+        Assert.Multiple(() => {
+          Assert.That(locations[i].Line, Is.EqualTo(expected[i].Line));
+          Assert.That(locations[i].Column, Is.EqualTo(expected[i].Column));
+          Assert.That(locations[i].Offset, Is.EqualTo(expected[i].Offset));
+        });
+      }
+    });
+  }
+
+  private static List<Location> ReadLocations(string input) {
+    var source = new ProgramSource(new StringReader(input));
+    var locations = new List<Location>();
+    while (!source.IsEof) {
+      locations.Add(source.Location);
+      source.Next();
+    }
+    return locations;
+  }
+
+  #endregion
 }
